31e488b63c6033785ef868811ee553ec637b4613
QuangHieu <quanghieupc152002@gmail.com>
2024-11-19 20:32:56 +0700

    all file

---
+++ lib/config.ts
@@ -0,0 +1,9 @@
+const config = {
+  tbServer: "thingsboard.cloud",
+  // tbServer: "212.38.94.144:8081",
+  //tbServer: "212.38.94.144",
+  // tbServer: "thingsboard.srv524038.hstgr.cloud",
+  deviceId: "deeb0400-9d16-11ef-97bd-1f25be15eb58",
+};
+
+export { config };
